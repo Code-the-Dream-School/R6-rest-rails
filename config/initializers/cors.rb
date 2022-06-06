a270f16b07a59ba5dae9e3f75624af5b92096368
CeeFish <can.o.fisher@gmail.com>
2022-06-06 11:05:47 -0400

    Creating API server application and with the Devise Secret Key.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,18 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+# THis allows your server to accept REST requests from any origin. This is not a good general practice 
+# — you would want to specify the address of your front end application instead of * 
+# — but CORS is outside the scope of this lesson.
+# Using origins '*' is for our convenience. When deploying to production, set origins to the URL of your frontend app. 
+# Otherwise the whole internet will be able to hit your API. 
+# Though in some cases that’s desirable.
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
