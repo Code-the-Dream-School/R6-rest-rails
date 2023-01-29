c9139d1bd2cee6ebc398ee7883241ef0ee901ca9
Cowboy <cbrobak79@gmail.com>
2023-01-28 20:56:03 -0500

    Updated files to fix error.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :email, presence: true, uniqueness: true
-
+​
   devise :database_authenticatable,
          :jwt_authenticatable,
          :registerable,
