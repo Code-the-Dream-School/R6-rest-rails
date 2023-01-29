ff8f18d40474086aae1b2af9ab4719c927299923
Cowboy <cbrobak79@gmail.com>
2023-01-28 21:38:26 -0500

    Final commit. Project should be complete.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :email, presence: true, uniqueness: true
-​
+  
   devise :database_authenticatable,
          :jwt_authenticatable,
          :registerable,
